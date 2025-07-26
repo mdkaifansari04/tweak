46a4b34d4128980c1a356cc653e0a8e8106a8fc2
Md Kaif Ansari <amdkaif843@gmail.com>
2025-07-26 16:20:01 +0530

    (feat+fix): - add babel config to fix dev error - add full-screen loader

--- components/layout/initial-layout.tsx
+++ components/layout/initial-layout.tsx
@@ -1,6 +1,7 @@
 import { useAuth } from "@clerk/clerk-expo";
 import { Stack, useRouter, useSegments } from "expo-router";
 import { useEffect } from "react";
+import FullScreenLoader from "../ui/full-screen-loader";
 
 export default function InitialLayout() {
   const { isLoaded, isSignedIn } = useAuth();
@@ -12,5 +13,7 @@ export default function InitialLayout() {
     if (!inAuthGroup && !isSignedIn) router.replace("/(auth)/login");
     else if (inAuthGroup && isSignedIn) router.replace("/(tabs)");
   }, [isLoaded, isSignedIn, segment, router]);
-  if (!isLoaded) return <Stack screenOptions={{ headerShown: false }} />;
+  if (!isLoaded) return <FullScreenLoader />;
+
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
---
+++ components/ui/full-screen-loader.tsx
@@ -0,0 +1,33 @@
+import { Loader } from "lucide-react-native";
+import React, { useEffect, useRef } from "react";
+import { Animated, Easing, View } from "react-native";
+
+export default function FullScreenLoader() {
+  const spinAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.loop(
+      Animated.timing(spinAnim, {
+        toValue: 1,
+        duration: 1000,
+        easing: Easing.linear,
+        useNativeDriver: true,
+      })
+    ).start();
+  }, [spinAnim]);
+
+  const spin = spinAnim.interpolate({
+    inputRange: [0, 1],
+    outputRange: ["0deg", "360deg"],
+  });
+
+  return (
+    <View
+      style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#fff" }}
+    >
+      <Animated.View style={{ transform: [{ rotate: spin }] }}>
+        <Loader size={32} />
+      </Animated.View>
+    </View>
+  );
+}
